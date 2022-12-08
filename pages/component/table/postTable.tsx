c1805855238c520ea7a942e9e93f03a92b9b31a7
Muhammad Haidar <62695274+DrLaeddis@users.noreply.github.com>
2022-12-08 08:56:24 +0700

    add table post

--- pages/component/table/postTable.tsx
+++ pages/component/table/postTable.tsx
@@ -1,8 +1,47 @@
 export default function PostTable() {
+
+    const someIndex = [];
+    for(var i = 1; i <= 10; i++) {
+        someIndex.push({
+            key: i.toString(),
+            No: i,
+            PostTitle: `Content Name ${i}`,
+            PostContent: `Post Content ${i}`,
+            PostDesc: `Post Description ${i}`,
+            Date: `Date ${i}`,
+            Admin: `Hendro ${i}`,
+        });
+    }
+
     return(
     <>
-        <div className="">
+        <div className="contentTable">
             <p>this is post table</p>
+            <table>
+                <tbody>
+                    <tr>
+                        <th>No</th>
+                        <th>Post Title</th>
+                        <th>Post Content</th>
+                        <th>Post Description</th>
+                        <th colSpan={2}>Option</th>
+                    </tr>
+
+                    {
+                        someIndex.map((data)=>{
+                            return(
+                            <tr key={data.key}>
+                                <td>{data.No}</td>
+                                <td>{data.PostTitle}</td>
+                                <td>{data.PostContent}</td>
+                                <td>{data.PostDesc}</td>
+                                <td>Detail | Edit | Delete</td>
+                            </tr>
+                            )
+                        })
+                    }
+                </tbody>
+            </table>
         </div>
     </>
     )
